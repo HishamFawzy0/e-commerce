412cd9410a10fbea33e7f386f25f8c8b1b0725f0
HishamFawwzy0 <hishamfawzy16@gmail.com>
2025-02-07 21:50:49 +0200

    reigester changes

--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import{FormControl, FormGroup, ReactiveFormsModule}from '@angular/forms';
+import{FormControl, FormGroup, ReactiveFormsModule, Validators}from '@angular/forms';
 @Component({
   selector: 'app-register',
   imports: [ReactiveFormsModule],
@@ -8,14 +8,27 @@ import{FormControl, FormGroup, ReactiveFormsModule}from '@angular/forms';
 })
 export class RegisterComponent {
   registerForm: FormGroup = new FormGroup({
-    name: new FormControl(null),
-    email: new FormControl(null),
-    password: new FormControl(null),
-    rePassword: new FormControl(null),
-    phone: new FormControl(null),
+    name: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(20),
+    ]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(/^[A-Z][a-zA-Z0-9]{5,10]$/),
+    ]),
+    rePassword: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(/^[A-Z][a-zA-Z0-9]{5,10]$/),
+    ]),
+    phone: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(/^(01)[0125][0-9]{8}$/),
+    ]),
   });
 
-  register():void{
-    alert('Registered successfully');
+  register(): void {
+    console.log(this.registerForm);
   }
-} 
+}
