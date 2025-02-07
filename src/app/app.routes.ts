95b0c401ab457ff97025e77d8595404caeba3397
HishamFawwzy0 <hishamfawzy16@gmail.com>
2025-02-07 21:20:49 +0200

    firstCommit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/pages/home/home.component';
+import { CartComponent } from './features/pages/cart/cart.component';
+import { ProductsComponent } from './features/pages/products/products.component';
+import { CategoriesComponent } from './features/pages/categories/categories.component';
+import { BrandsComponent } from './features/pages/brands/brands.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
+import { NotfoundComponent } from './features/layout/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'register', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'brands', component: BrandsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: NotfoundComponent },
+];
---
+++ src/app/features/auth/register/register.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import{FormControl, FormGroup, ReactiveFormsModule}from '@angular/forms';
+@Component({
+  selector: 'app-register',
+  imports: [ReactiveFormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
+})
+export class RegisterComponent {
+  registerForm: FormGroup = new FormGroup({
+    name: new FormControl(null),
+    email: new FormControl(null),
+    password: new FormControl(null),
+    rePassword: new FormControl(null),
+    phone: new FormControl(null),
+  });
+
+  register():void{
+    alert('Registered successfully');
+  }
+} 
